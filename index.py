17f15231b8907315302e91d211b899ce5d9668ed
baniu.yao@gmail.com <baniu.yao@gmail.com>
2014-03-25 22:21:27 +0800

    change test TOKEN

--- index.py
+++ index.py
@@ -1,12 +1,14 @@
-from bottle import Bottle, request
+from bottle import Bottle, request, run
+import bottle
 import hashlib
 
+bottle.debug(True)
 app = Bottle()
 
-TOKEN='zabbix'
+TOKEN='frank'
 
 @app.route('/')
-def hello():
+def index():
     timestamp =  request.GET.get('timestamp', '')
     nonce = request.GET.get('nonce', '')
     wx_sha1_ret = request.GET.get('signature', '')
@@ -14,5 +16,9 @@ def hello():
     token = TOKEN
     l = [timestamp, nonce, token]
     l.sort()
-    sha1_ret = hashlib.sha1(''.join(l))
-    return sha1_ret + '?=' + wx_sha1_ret
+    sha1_ret = hashlib.sha1(''.join(l)).hexdigest()
+    return sha1_ret + '!=' + wx_sha1_ret
+
+
+if __name__ == '__main__':
+    run(app=app, host='localhost', port=8000, reloader=True)
