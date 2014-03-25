b38f3b1b8b1fb9cf3c29f2bff747d65a345ee776
baniu.yao@gmail.com <baniu.yao@gmail.com>
2014-03-25 22:10:21 +0800

    complete auth step

--- index.py
+++ index.py
@@ -1,7 +1,18 @@
-from bottle import Bottle, run
+from bottle import Bottle, request
+import hashlib
 
 app = Bottle()
 
+TOKEN='zabbix'
+
 @app.route('/')
 def hello():
-    return "Hello, world! - Bottle1"
+    timestamp =  request.GET.get('timestamp', '')
+    nonce = request.GET.get('nonce', '')
+    wx_sha1_ret = request.GET.get('signature', '')
+    echostr = request.GET.get('echostr', '')
+    token = TOKEN
+    l = [timestamp, nonce, token]
+    l.sort()
+    sha1_ret = hashlib.sha1(''.join(l))
+    return sha1_ret + '?=' + wx_sha1_ret
